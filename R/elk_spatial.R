05b0f39feb84163118d6aa695b71ee3472fd9ca3
Matt Dornback <matt.dornback@noaa.gov>
2023-04-20 21:12:34 +0000

    version 2.2.4.2

--- R/elk_spatial.R
+++ R/elk_spatial.R
@@ -2,15 +2,15 @@
 #'
 #' Shapefile for Elkhorn Slough reserve boundary
 #'
-#' @format A \code{\link[sp]{SpatialPolygons}} object
+#' @format A \code{\link[sf]{sf}} object
 #'
-#' @references NOAA National Estuarine Research Reserve System (NERRS). System-wide Monitoring Program. Data accessed from the NOAA NERRS Centralized Data Management Office website: http://www.nerrsdata.org/; accessed 08 October 2016
+#' @references NOAA National Estuarine Research Reserve System (NERRS). System-wide Monitoring Program. Data accessed from the NOAA NERRS Centralized Data Management Office website: https://www.nerrsdata.org/; accessed 08 October 2016
 #'
 #' @docType data
 #' @keywords datasets
 #' @name elk_spatial
 #' @usage data(elk_spatial)
-#' @source \href{http://cdmo.baruch.sc.edu/get/gis.cfm}{CDMO}
+#' @source \href{https://cdmo.baruch.sc.edu/get/gis.cfm}{CDMO}
 #'
 
 "elk_spatial"
